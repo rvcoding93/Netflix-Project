2ecb119a1f6828b74bfcace8780f1df531c7ada8
Enoch <enoch4kigwila@gmail.com>
2022-05-16 14:10:56 -0400

    adding home.css file and renders to each js in the components

---
+++ netflix-page/src/components/Content.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Content extends React.Component {
+
+    render(){
+        return (
+            
+            <div className=''>
+
+                <img src=''/>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Content;
---
+++ netflix-page/src/components/LogIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Login extends React.Component {
+
+    render(){
+        return (
+            
+            <div className=''>
+
+                <img src=''/>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Login;
